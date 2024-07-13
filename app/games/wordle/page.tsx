bbc25475fc65153846f1b8379a38d8c10c8ea542
Clee <christopherlee2012@gmail.com>
2024-07-13 16:05:13 -0400

    fix css

--- app/games/wordle/page.tsx
+++ app/games/wordle/page.tsx
@@ -2,18 +2,20 @@ const workBankWork = "Shelf";
 
 const LetterBox = ({ letter }: { letter: string }) => {
   return (
-    <div className={"border-2 rounded border-black w-10 h-10"}>
-      <h1>{letter}</h1>
+    <div
+      className={
+        "border-2 rounded border-black w-10 h-10 items-center justify-center"
+      }
+    >
+      <h1 className={"align-middle"}>{letter}</h1>
     </div>
   );
 };
 export default function WordlePage() {
   return (
     <div>
-      <h1 className={"border-2 m-10 rounded border-black w-10 h-10"}>
-        Wordle Home Page
-      </h1>
-      {/*<LetterBox letter={"a"} />*/}
+      <h1>Wordle Home Page</h1>
+      <LetterBox letter={"a"} />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useRouter } from "next/navigation";
-import "../styles/global.css";
 
 const HomePage = () => {
   const router = useRouter();
