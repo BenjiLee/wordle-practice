8533a96f583f5715945884a7f17ff358ffbe270d
Clee <christopherlee2012@gmail.com>
2024-07-13 15:35:28 -0400

    Setup wordle page

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <h1>Games Home Page</h1>
-}
+"use client";
+import { useRouter } from "next/navigation";
+
+const HomePage = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/games/wordle");
+  };
+
+  return (
+    <div>
+      <h1>Games Gome Page</h1>
+      <button onClick={handleClick}>Play Wordle</button>
+    </div>
+  );
+};
+
+export default HomePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-}
+"use client";
+import { useRouter } from "next/navigation";
+
+const HomePage = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/games");
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={handleClick}>Go To Games Home</button>
+    </div>
+  );
+};
+
+export default HomePage;
