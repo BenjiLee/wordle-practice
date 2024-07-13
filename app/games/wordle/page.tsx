ad4cc9b93a05ffb9660030bffc151c2f54a5e08f
Clee <christopherlee2012@gmail.com>
2024-07-13 17:11:37 -0400

    MVP Wordle

--- app/games/wordle/page.tsx
+++ app/games/wordle/page.tsx
@@ -1,21 +1,175 @@
-const workBankWork = "Shelf";
+"use client";
+import { SetStateAction, useEffect, useState } from "react";
 
-const LetterBox = ({ letter }: { letter: string }) => {
+const workBankWork = "Shelfy";
+const DEBUG = true;
+const LetterBox = ({
+  answer,
+  column,
+  reveal,
+  disabled,
+  setResult,
+  word,
+}: {
+  answer: string;
+  column: number;
+  reveal: boolean;
+  disabled: boolean;
+  setResult: React.Dispatch<SetStateAction<{}>>;
+  word: string;
+}) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event: { target: { value: string } }) => {
+    if (event.target.value.length > 1) {
+      return;
+    }
+    const upperCaseValue = event.target.value.toUpperCase();
+    setValue(upperCaseValue);
+    if (upperCaseValue === "") {
+      setResult((prev) => {
+        return {
+          ...prev,
+          [column]: null,
+        };
+      });
+      return;
+    }
+    setResult((prev) => {
+      return {
+        ...prev,
+        [column]: upperCaseValue === word[column].toUpperCase(),
+      };
+    });
+  };
+  let style = "bg-gray-300";
+  if (reveal) {
+    if (value.toUpperCase() === answer.toUpperCase()) {
+      style = "bg-green-500";
+    } else if (
+      workBankWork.toUpperCase().split("").includes(value.toUpperCase())
+    ) {
+      style = "bg-yellow-500";
+    } else {
+      style = "bg-gray-400";
+    }
+  }
   return (
-    <div
-      className={
-        "border-2 rounded border-black w-10 h-10 items-center justify-center"
+    <>
+      <div
+        className={
+          "border-2 m-1 rounded border-black w-10 h-10 flex items-center justify-center " +
+          style
+        }
+      >
+        <input
+          type="text"
+          value={value}
+          disabled={disabled}
+          onChange={handleChange}
+          className={
+            "border-2 text-4xl w-10  flex items-center justify-center " + style
+          }
+        />
+      </div>
+      {DEBUG && (
+        <>
+          {disabled && "D"}
+          {reveal && "R"}
+          {answer}
+        </>
+      )}
+    </>
+  );
+};
+
+const LetterBoxRow = ({
+  word,
+  currentRow,
+  row,
+  setRowState,
+}: {
+  word: string;
+  currentRow: number;
+  row: number;
+  setRowState: (rowState: RowState) => void;
+}) => {
+  const shouldReveal = row < currentRow;
+  const disabled = row > currentRow || shouldReveal;
+
+  const [result, setResult] = useState({});
+
+  useEffect(() => {
+    for (const value of Object.values(result)) {
+      if (value === null) {
+        setRowState("incomplete");
+        return;
       }
-    >
-      <h1 className={"align-middle"}>{letter}</h1>
+      if (value === false) {
+        setRowState("wrong");
+        return;
+      }
+    }
+    setRowState("correct");
+  }, [result]);
+
+  if (row === currentRow) {
+    console.log(JSON.stringify(result));
+  }
+  return (
+    <div className={"flex flex-row"}>
+      {word.split("").map((answer, index) => (
+        <LetterBox
+          key={"letterBox" + row + answer}
+          column={index}
+          answer={answer}
+          reveal={shouldReveal}
+          disabled={disabled}
+          setResult={setResult}
+          word={word}
+        />
+      ))}
     </div>
   );
 };
+
+const rows = [0, 1, 2, 3, 4];
+
+type RowState = "incomplete" | "wrong" | "correct";
 export default function WordlePage() {
+  const [currentRow, setCurrentRow] = useState(0);
+  const [rowState, setRowState] = useState<RowState>("incomplete");
+
+  const onCheckWord = () => {
+    switch (rowState) {
+      case "incomplete":
+        return;
+      case "wrong":
+        setCurrentRow(currentRow + 1);
+        return;
+      case "correct":
+        alert("You win!");
+        break;
+    }
+  };
+
   return (
     <div>
       <h1>Wordle Home Page</h1>
-      <LetterBox letter={"a"} />
+      <div className={"flex flex-col"}>
+        {rows.map((row, index) => (
+          <LetterBoxRow
+            word={workBankWork}
+            key={index}
+            currentRow={currentRow}
+            row={index}
+            setRowState={setRowState}
+          />
+        ))}
+        <button className={"border-2 w-14 m-5"} onClick={onCheckWord}>
+          Next Row
+        </button>
+      </div>
     </div>
   );
 }
